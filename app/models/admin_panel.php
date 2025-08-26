a7c118eb1b66ed336fd05dc182b881eca711dbcc
j0nathan <kuba.jan@prumkadc.cz>
2025-08-26 20:33:16 +0200

    Upraveny modely - jeste nejsou hotove. Dodelat!

--- app/models/admin_panel.php
+++ app/models/admin_panel.php
@@ -6,12 +6,8 @@ class admin_panel extends \DB\Cortex
         $table = 'admin_panel';
     protected $fieldConf=[
         'call_request'=>[
-            'type'=>'VARCHAR128',
+            'belongs-to-one'=>'models\call_request',
             'required'=>true
-        ],
-        'review_text'=>[
-            'type'=>'VARCHAR256',
-            'default'=>0
         ],
         'rating'=>[
             'type'=>'INT',
