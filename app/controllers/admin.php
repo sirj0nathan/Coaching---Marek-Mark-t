b62c35a9ec70ee644b9c595e4182e83bd992f07b
j0nathan <kuba.jan@prumkadc.cz>
2025-09-23 15:04:28 +0200

    Upraveno plno dalsich veci - jeste je potreba hodne zmenit

--- app/controllers/admin.php
+++ app/controllers/admin.php
@@ -78,7 +78,49 @@ class admin
 
             \Flash::instance()->addMessage('Služba byla aktualizována', 'success');
         }
-
         $base->reroute('/admin/services');
+    }
+
+    public function getEditUser(\Base $base, $params){
+        $user = new \models\user();
+        $user->load(['id=?', $params['id']]);
+        if ($user->dry()) {
+            \Flash::instance()->addMessage('Uživatel nenalezen', 'danger');
+            $base->reroute('/admin/users');
+            return;
+        }
+        $base->set('user', $user);
+        $base->set('title', 'Úprava uživatele');
+        $base->set('content', 'admin/edit_user.html');
+        echo \Template::instance()->render('index.html');
+    }
+
+    public function postEditUser(\Base $base, $params)
+    {
+        if (!$base->get('SESSION.uid') || $base->get('SESSION.role') !== 'admin') {
+            \Flash::instance()->addMessage('Nemáte oprávnění', 'danger');
+            $base->reroute('/');
+            return;
+        }
+
+        $user = new \models\user();
+        $user->load(['id=?', $params['id']]);
+
+        if ($user->dry()) {
+            \Flash::instance()->addMessage('Uživatel nenalezen', 'danger');
+            $base->reroute('/admin/users');
+            return;
+        }
+
+        $user->name = $base->get('POST.name');
+        $user->email = $base->get('POST.email');
+        if ($base->get('POST.password')) {
+            $user->password = password_hash($base->get('POST.password'), PASSWORD_BCRYPT);
+        }
+        $user->role = $base->get('POST.role');
+        $user->save();
+
+        \Flash::instance()->addMessage('Uživatel byl aktualizován', 'success');
+        $base->reroute('/admin/users');
     }
 }
