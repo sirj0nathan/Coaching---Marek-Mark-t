281bd25ca75b3cfa00ed73f3dcfefb0b232475cb
j0nathan <kuba.jan@prumkadc.cz>
2025-09-23 14:16:39 +0200

    Prepsane nazvy souboru pro lepsi orientaci na strankach

--- app/controllers/index.php
+++ app/controllers/index.php
@@ -18,7 +18,7 @@ class index
         $base->set('reviews', $reviews ?: []);
 
         $base->set('title', 'O nás');
-        $base->set('content', 'home/about.html');
+        $base->set('content', '/home/index.html');
         echo \Template::instance()->render('index.html');
     }
 }
